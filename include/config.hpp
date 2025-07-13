6c1e6747623cd57a1d77475d3787bd7eb8fcd117
Grafscorp <golyandringraf@gmail.com>
2025-07-14 00:38:07 +0300

    Release oled config pins

--- include/config.hpp
+++ include/config.hpp
@@ -7,6 +7,12 @@ namespace HardwareConfig{
 constexpr uint8_t BUTTON_PINS[] = {2,3,4,5};
 constexpr uint8_t OLED_SDA = A4;
 constexpr uint8_t OLED_SDL = A5;
+//OLED DISPLAY CONFIGS
+constexpr uint8_t OLED_WIDTH = 128;
+constexpr uint8_t OLED_HEIGHT = 64;
+constexpr uint8_t OLED_ADDRESS = 0x3C;
+
+//SERIAL PORT
 constexpr uint32_t SERIAL_BAUD_RATE = 115200;
 }
 #endif
