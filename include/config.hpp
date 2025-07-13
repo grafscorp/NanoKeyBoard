e36cc2b2954eacc02cbf0c07a97cb2557ea9142b
Grafscorp <golyandringraf@gmail.com>
2025-07-14 02:26:34 +0300

    Update comments

--- include/config.hpp
+++ include/config.hpp
@@ -20,18 +20,23 @@ constexpr uint32_t SERIAL_BAUD_RATE = 115200;
 
 
 namespace ButtonsConfig{
-//Buttons
+//Buttons size
 constexpr uint8_t BUTTONS_SIZE = sizeof(HardwareConfig::BUTTON_PINS);
 
 //Buttons actions
 enum ButtonDefault : uint8_t{
-    TOGGLE_PLAY =1,
-    PREV_MEDIA,
-    NEXT_MEDIA,
-    CUSTOM_ACTION
+    TOGGLE_PLAY =1, //Button 1: On/off
+    PREV_MEDIA,     //Button 2: Preview 
+    NEXT_MEDIA,     //Button 3: Next
+    CUSTOM_ACTION   //Button 4: Programming
 };
+    //Buttons debounce
+
+    //Количество стабильных чтений для фиксации
     constexpr uint8_t DebounceThreshold=2;
+    // Интервал между чтениями
     constexpr uint8_t DebounceIntervalMs=2;
+    // Длительное нажатие (мс)
     constexpr uint8_t LongPressDuration=1000;
 }
 
