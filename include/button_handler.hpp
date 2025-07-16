5292d8865da75cd0c167a7d8525b6a9971a4f56a
Grafscorp <golyandringraf@gmail.com>
2025-07-16 23:37:36 +0300

    Update button_handler.hpp

--- include/button_handler.hpp
+++ include/button_handler.hpp
@@ -5,6 +5,8 @@
 #include "config.hpp"
 
 
+#define POLLUP_INIT true
+#define WITHOUT_PULLUPINIT false
 
 /// @brief 
 class ButtonHandler
@@ -12,12 +14,21 @@ class ButtonHandler
 public:
     /// @brief 
     ButtonHandler();
-    /// @brief Функция инициализации пинов для кнопок. 
-    //По умолчанию подключаются со втсроенными подтягивающими резисторами.  
-    void init();
+    /* @brief Функция инициализации пинов для кнопок. 
+    /По умолчанию подключаются со втсроенными подтягивающими резисторами.
+    @param POLLUPINIT (по умолчанию) подключть кнопки со встроенным  подтягивающим резистором
+    @param WITHOUT_PULLUPINIT подключить кнопки без встроенных подтягивающих резисторов
+    */  
+    void init(const bool = POLLUP_INIT);
+
     /// @brief 
     void update();
-    
+
+    void getButtonsStateData();
+
+private:
+//Данные: Нажата кнопка(true) или нет (false)
+    bool buttonsPressed[ButtonsConfig::BUTTONS_COUNT];
 };
 
 
