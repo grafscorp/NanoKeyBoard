7f08aba3bfe4f3cc694684f9a5e76fd94af37c5f
Grafscorp <golyandringraf@gmail.com>
2025-07-17 18:01:51 +0300

    Создал DisplayConfig и перенёс туда данные из HardwareConfig

--- include/config.hpp
+++ include/config.hpp
@@ -7,14 +7,6 @@ namespace HardwareConfig{
     constexpr uint8_t BUTTON_PINS[] = {2,3,4,5};
     constexpr uint8_t OLED_SDA = A4;
     constexpr uint8_t OLED_SDL = A5;
-    //OLED DISPLAY CONFIGS
-    constexpr uint8_t OLED_WIDTH = 128;
-    constexpr uint8_t OLED_HEIGHT = 64;
-    constexpr uint8_t OLED_ADDRESS = 0x3C;
-
-    //SERIAL PORT
-    constexpr uint32_t SERIAL_BAUD_RATE = 115200;
-    constexpr uint16_t SERIAL_TIMEOUT_MS = 100;
 
     //Potenciometer
     constexpr uint8_t VolumePotPin = A0;         // Аналоговый пин
@@ -31,4 +23,10 @@ namespace ButtonsConfig{
 
 
 
+namespace DisplayConfig{
+    //OLED DISPLAY CONFIGS
+    constexpr uint8_t OLED_WIDTH = 128;
+    constexpr uint8_t OLED_HEIGHT = 64;
+    constexpr uint8_t OLED_ADDRESS = 0x3C;
+}
 #endif
