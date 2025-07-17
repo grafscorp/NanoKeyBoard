3c9e1e53ec4bd48f8c2df4c65de48177dc1acd88
Grafscorp <golyandringraf@gmail.com>
2025-07-17 23:25:28 +0300

    Release sendData by ISerialDataSource

--- include/comm/serial_protocol.hpp
+++ include/comm/serial_protocol.hpp
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 #include "config.hpp"
-
+#include "interface/I_serial_data_source.hpp"
 
 class SerialProtocol
 {
@@ -11,13 +11,13 @@ public:
     SerialProtocol();
     void init();
     void update();
-    void sendPacket(const uint8_t cmd, const uint8_t data);
-
+    
+    void sendData(const ISerialDataSource& source);
 private:
     //Aлгоритм CRC-8 — простую контрольную сумму для обнаружения ошибок в последовательности байт.
     //Target - Вычислить 1-байтовую CRC-8 контрольную сумму для переданных данных, используя полином 0x07, что соответствует стандартному CRC-8 (также известному как CRC-8-ATM или CRC-8-ITU).
     uint8_t crc8(const uint8_t *data, size_t len);
-
+    void sendPacket(const uint8_t cmd, const uint8_t data);
 };
 
 
