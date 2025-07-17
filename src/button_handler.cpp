9bd9e750aae9174d33d517262e43d8e9098e0203
Grafscorp <golyandringraf@gmail.com>
2025-07-17 18:04:16 +0300

    Update algorythm for button data pressed

--- src/button_handler.cpp
+++ src/button_handler.cpp
@@ -14,7 +14,6 @@ ButtonHandler::ButtonHandler() {
 
 void ButtonHandler::init(const bool initPullUp)
 {
-    for (auto buttonPin : ButtonsConfig::BUTTON_PINS)
     for (auto buttonPin : HardwareConfig::BUTTON_PINS)
     {
         pinMode(buttonPin, initPullUp?INPUT_PULLUP:INPUT );
@@ -24,10 +23,17 @@ void ButtonHandler::init(const bool initPullUp)
 
 void ButtonHandler::update()
 {
-    for (uint8_t i = 0; i < ButtonsConfig::BUTTONS_COUNT; i++)
+    // for (uint8_t i = 0; i < ButtonsConfig::BUTTONS_COUNT; i++)
+    // {
+    //     //Записываем состояние кнопки, так как по умолчанию стоит подтягивающий резистор, значение true с кнопки означает что она не нажата
+    //     buttonsPressed[i] = !(static_cast<bool>(digitalRead(HardwareConfig::BUTTON_PINS[i])));
+    //     0b00;
+    // }
+    this->buttonData = 0U;
+    for (uint8_t i = ButtonsConfig::BUTTONS_COUNT-1; i >=0; --i)
     {
-        //Записываем состояние кнопки, так как по умолчанию стоит подтягивающий резистор, значение true с кнопки означает что она не нажата
-        buttonsPressed[i] = !(static_cast<bool>(digitalRead(ButtonsConfig::BUTTON_PINS[i])));
+        buttonData <<= 1U;
+        buttonData |= (digitalRead(HardwareConfig::BUTTON_PINS[i]) & 1U);
     }
     
     
