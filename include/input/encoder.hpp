e19994b7b3232ae9844ac4b064d62e1404437f20
Grafscorp <golyandringraf@gmail.com>
2025-07-18 13:15:57 +0300

    Update encoder.hpp

--- include/input/encoder.hpp
+++ include/input/encoder.hpp
@@ -12,9 +12,23 @@ public:
     virtual ~Encoder() = default;
     bool init();
     bool init(const uint8_t pinA, const uint8_t pinB, const uint8_t pulsePerStep = 4 );
-
+    void update();
+    int8_t getDirection() const;
+    int16_t getSteps() const;
+    bool isRotated() const;
+    void resetSteps();
 private:
+    uint8_t pinA ;
+    uint8_t pinB ;
+    uint8_t pulsePerStep=4;
     
+
+    volatile int8_t lastState =0;
+    volatile int16_t steps = 0;
+    volatile int8_t direction = 0 ;
+    volatile bool rotated = false;
+
+    uint8_t readState() const;
 };
 
 
