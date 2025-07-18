58c5d0381ade1b1902b89d75b9ebc66057a7e492
Grafscorp <golyandringraf@gmail.com>
2025-07-18 21:32:47 +0300

    Fix pins for buttons and encoder

--- include/config.hpp
+++ include/config.hpp
@@ -4,7 +4,7 @@
 
 namespace HardwareConfig{
     //PINS CONFIGS
-    constexpr uint8_t BUTTON_PINS[] = {2,3,4,5};
+    constexpr uint8_t BUTTON_PINS[] = {4,5,6,7};
     constexpr uint8_t OLED_SDA = A4;
     constexpr uint8_t OLED_SDL = A5;
 
@@ -22,9 +22,13 @@ namespace ButtonsConfig{
 
     //Debounce Delay for Cherry MX
     constexpr const unsigned long DebounceDelay = 20;
+
 }
 
-
+namespace EncoderConfig{
+    constexpr uint8_t pinA = 2;
+    constexpr uint8_t pinB = 3;
+}
 
 namespace SerialConfig{
     //SERIAL PORT
@@ -49,12 +53,44 @@ namespace ProtocolConfig{
         CMD_DISPLAY = 0x04
     };
 
-    struct Packet
-    {
-        uint8_t length;
-        COMMANDS cmds;
-        uint8_t data;
-    };
+    // #pragma pack(1)
+    // struct Packet
+    // {
+    //     uint8_t length;
+    //     COMMANDS cmds;
+    //     uint8_t data;
+    // };
+    // #pragma pack(pop)
+    //TEST SERIAL PROTOCOL
+    //8 byte 
+    /*
+    0xff -      start byte
+    0x00 -      length
+    0x01-0x40 - command {system, buttons, volume, display}
+    0x00-0xff -      payload{type logs, num button, canhed?, ...}
+    0x -        CRC
+    0xFE -      end byte
+
+
+    BUTTONS EXAMPLE
+
+    0xff        start
+    0x02        length
+    0x02        buttons
+    0x0000_0001 0 button
+    0xcrc       (0x10, 0x01)
+    0xFE        end
+
+    VOLUME EXAMPLE
+
+    0xff -      start
+    0x          length
+    0x03        volume
+    0x00-0x10-0x20   data (0, -1 ,+1)
+    0x          crc
+    0xfe        end
+
+    */ 
     
 }
 namespace DisplayConfig{
@@ -63,25 +99,5 @@ namespace DisplayConfig{
     constexpr uint8_t OLED_HEIGHT = 64;
     constexpr uint8_t OLED_ADDRESS = 0x3C;
 }
-//TEST SERIAL PROTOCOL
-//8 byte 
-/*
-0xff -      start byte
-0x00 -      length
-0x01-0x40 - command {system, buttons, volume, display}
-0x00-0xff -      payload{type logs, num button, canhed?, ...}
-0x -        CRC
-0xFE -      end byte
 
-
-BUTTONS EXAMPLE
-
-0xff - start
-0x02     length
-0x01 - buttons
-0x0000_0001 - 0 button
-0xcrc  (0x10, 0x01)
-0xFE -end
-
-*/ 
 #endif
