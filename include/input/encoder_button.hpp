b01075ad0e978abb790028d7c4aeb1781fe77827
Grafscorp <golyandringraf@gmail.com>
2025-07-21 02:04:35 +0300

    Release SerialDataSource Interface in EncoderButton

--- include/input/encoder_button.hpp
+++ include/input/encoder_button.hpp
@@ -2,7 +2,8 @@
 #define ENCODER_BUTTON_HPP
 #include "input/encoder.hpp"
 #include "input/debounced_button.hpp"
-class EncoderButton 
+
+class EncoderButton : public ISerialDataSource
 {
 public:
     explicit EncoderButton(const uint8_t pinButton,const uint8_t pinA=2, const uint8_t pinB=3,const unsigned long debounceButtonDelay = 50 );
@@ -14,7 +15,11 @@ public:
     bool getEncoderChanged() ;
     int8_t getEncoderSteps() ;
     int8_t getEncoderDirection() ;
+    const uint8_t getSerialData() const override;
+    const uint8_t getSerialCommand() const override;
+    void updateSerialData() override;
 private:
+    uint8_t data = 0x00;
     Encoder encoder;
     DebouncedButton button;
     bool buttonChanged = false;
